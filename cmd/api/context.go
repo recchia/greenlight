adecf10a050e77e1ad70f39c5fba038ea35269dd
Piero Recchia <piero.recchia@gmail.com>
2026-02-12 00:19:56 -0300

    Added authentication and authorization middleware, implement stateful token management and create migration for tokens table.

---
+++ cmd/api/context.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/recchia/greenlight/internal/data"
+)
+
+type contextKey string
+
+const userContextKey = contextKey("user")
+
+func (app *application) contextSetUser(r *http.Request, user *data.User) *http.Request {
+	ctx := context.WithValue(r.Context(), userContextKey, user)
+
+	return r.WithContext(ctx)
+}
+
+func (app *application) contextGetUser(r *http.Request) *data.User {
+	user, ok := r.Context().Value(userContextKey).(*data.User)
+	if !ok {
+		panic("user not found in request context")
+	}
+
+	return user
+}
