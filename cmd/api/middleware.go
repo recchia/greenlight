3d254916a96425a1c5fceaaaac5eaae5cc92b1cb
Piero Recchia <piero.recchia@gmail.com>
2026-02-24 01:50:13 -0300

    added custom metrics middleware to app

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"expvar"
 	"fmt"
 	"net/http"
 	"strings"
@@ -194,5 +195,21 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 		}
 
 		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) metrics(next http.Handler) http.Handler {
+	var (
+		totalRequestsReceived           = expvar.NewInt("total_requests_received")
+		totalResponsesSent              = expvar.NewInt("total_responses_sent")
+		totalProcessingTimeMicroseconds = expvar.NewInt("total_processing_time_microseconds")
+	)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		totalRequestsReceived.Add(1)
+		next.ServeHTTP(w, r)
+		totalResponsesSent.Add(1)
+		totalProcessingTimeMicroseconds.Add(int64(time.Since(start) / time.Microsecond))
 	})
 }
